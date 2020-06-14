60beb640564c4db8ad3eec99d7e731670ab88ac4
yoseychan <laumons05@gmail.com>
2020-06-14 22:39:10 +0200

    ...

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,92 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <div class="container">
+        <router-link class="logo" to="/">Luunaro</router-link>
+        <div class="right">
+          <router-link to="/neumorphism">Neumorphism</router-link> |
+          <router-link to="/example">Examples</router-link> |
+          <router-link to="/about">About</router-link>
+        </div>
+      </div>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #15151B;
+  color: #F1F1F1;
+
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  h1 {
+    font-size: 44px;
+    color: #F5F2F2;
+    font-weight: 900;
+  }
+  h2 {
+    font-size: 34px;
+    color: #F1F1F1;
+  }
+  h3 {
+    font-size: 24px;
+    color: #F1F1F1;
+  }
+  h4 {
+    font-size: 18px;
+    color: #ADADAD;
+  }
+  h5 {
+    font-size: 12px;
+    color: #979797;
+  }
+  h6 {
+    font-size: 10px;
+    color: #6B6B6B;
+    text-transform: uppercase;
+  }
+  p{
+    font-size: 16px;
+    color: #C4C4C4;
+  }
 }
 
 #nav {
-  padding: 30px;
+  // padding: 20px;
+  .container {
+    max-width: 1600px;
+    margin: 0 auto;
+    overflow: hidden;
+  }
+  .logo {
+    float: left;
+    font-family: 'Courier New', Courier, monospace;
+    font-size: 44px;
+    font-weight: 900;
+    padding :0;
+    color: #c43282;
 
+  }
+  .right {
+    float: right;
+  }
   a {
     font-weight: bold;
     color: #2c3e50;
+    text-decoration: none;
+    padding:15px;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #c43282;
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,21 @@ Vue.use(VueRouter)
     component: Home
   },
   {
+    path: '/Neumorphism',
+    name: 'Neumorphism',
+    component: () => import(/* webpackChunkName: "neumorphism" */ '../views/Neumorphism.vue')
+  },
+  {
+    path: '/examples',
+    name: 'Examples',
+    component: () => import(/* webpackChunkName: "examples" */ '../views/Examples.vue')
+  },
+  {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+
 ]
 
 const router = new VueRouter({
